a158078aa22b5b9283db2ecea06611981d22bfb5
Tanmoy Adhikary <adhikary.tanmoy@gmail.com>
2020-05-25 16:57:20 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,8 @@
+// Add parameters we want
+vc_add_param('vc_row', array(
+    'type' => 'textfield',
+    'heading' => "Image URL",
+    'param_name' => 'lazy_bg',
+    'value' => '',
+    'description' => __("Lazy background image URL", "discprofile")
+));
