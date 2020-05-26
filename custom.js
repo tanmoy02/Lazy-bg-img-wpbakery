134f3ffff3a036da535e288cf79f9e22bd64e341
Tanmoy Adhikary <adhikary.tanmoy@gmail.com>
2020-05-26 13:04:36 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -2,9 +2,8 @@ function lazyBg() {
   var lazyImg = document.querySelectorAll('div[lazy-src]');
   var style = "background-image: url({url})";
   for (var i = 0; i < lazyImg.length; i++) {
-
-    lazyImg[i].setAttribute('style', style.replace("{url}", lazyImg[i].getAttribute('lazy-src')));
-
+      var oldStyle = lazyImg[i].getAttribute('style');
+      lazyImg[i].setAttribute('style', style.replace("{url}", lazyImg[i].getAttribute('lazy-src')) + '; ' + oldStyle);
   }
 }
 
